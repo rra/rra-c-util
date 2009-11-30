d127932b8711e89f753f99617d4ca41d911f220d
Russ Allbery <rra@stanford.edu>
2009-11-30 12:27:29 -0800

    Add additional GCC function attributes for utility functions
    Add the GCC function attributes alloc_size, malloc, and nonnull to the
    prototypes for utility functions where appropriate.

--- util/util.h
+++ util/util.h
@@ -41,23 +41,28 @@ struct sockaddr;
 #pragma GCC visibility push(hidden)
 
 /* Concatenate NULL-terminated strings into a newly allocated string. */
-char *concat(const char *first, ...);
+char *concat(const char *first, ...)
+    __attribute__((__malloc__, __nonnull__(1)));
 
 /*
  * Given a base path and a file name, create a newly allocated path string.
  * The name will be appended to base with a / between them.  Exceptionally, if
  * name begins with a slash, it will be strdup'd and returned as-is.
  */
-char *concatpath(const char *base, const char *name);
+char *concatpath(const char *base, const char *name)
+    __attribute__((__malloc__, __nonnull__(2)));
 
 /*
  * Like the non-x versions of the same function, but keep writing until either
  * the write is not making progress or there's a real error.  Handle partial
  * writes and EINTR/EAGAIN errors.
  */
-ssize_t xpwrite(int fd, const void *buffer, size_t size, off_t offset);
-ssize_t xwrite(int fd, const void *buffer, size_t size);
-ssize_t xwritev(int fd, const struct iovec *iov, int iovcnt);
+ssize_t xpwrite(int fd, const void *buffer, size_t size, off_t offset)
+    __attribute__((__nonnull__));
+ssize_t xwrite(int fd, const void *buffer, size_t size)
+    __attribute__((__nonnull__));
+ssize_t xwritev(int fd, const struct iovec *iov, int iovcnt)
+    __attribute__((__nonnull__));
 
 /*
  * Create a socket and bind it to the specified address and port (either IPv4
@@ -65,8 +70,10 @@ ssize_t xwritev(int fd, const struct iovec *iov, int iovcnt);
  * are reported using warn/syswarn.  To bind to all interfaces, use "any" or
  * "all" for address.
  */
-socket_type network_bind_ipv4(const char *address, unsigned short port);
-socket_type network_bind_ipv6(const char *address, unsigned short port);
+socket_type network_bind_ipv4(const char *address, unsigned short port)
+    __attribute__((__nonnull__));
+socket_type network_bind_ipv6(const char *address, unsigned short port)
+    __attribute__((__nonnull__));
 
 /*
  * Create and bind sockets for every local address (normally two, one for IPv4
@@ -75,7 +82,8 @@ socket_type network_bind_ipv6(const char *address, unsigned short port);
  * fds will be set to an array containing the resulting file descriptors, with
  * count holding the count returned.
  */
-void network_bind_all(unsigned short port, socket_type **fds, int *count);
+void network_bind_all(unsigned short port, socket_type **fds, int *count)
+    __attribute__((__nonnull__));
 
 /*
  * Create a socket and connect it to the remote service given by the linked
@@ -83,14 +91,16 @@ void network_bind_all(unsigned short port, socket_type **fds, int *count);
  * -1 on failure, with the error left in errno.  Takes an optional source
  * address.
  */
-socket_type network_connect(struct addrinfo *, const char *source);
+socket_type network_connect(struct addrinfo *, const char *source)
+    __attribute__((__nonnull__(1)));
 
 /*
  * Like network_connect but takes a host and port instead.  If host lookup
  * fails, errno may not be set to anything useful.
  */
 socket_type network_connect_host(const char *host, unsigned short port,
-                                 const char *source);
+                                 const char *source)
+    __attribute__((__nonnull__(1)));
 
 /*
  * Creates a socket of the specified domain and type and binds it to the
@@ -108,23 +118,27 @@ socket_type network_client_create(int domain, int type, const char *source);
  * Put an ASCII representation of the address in a sockaddr into the provided
  * buffer, which should hold at least INET6_ADDRSTRLEN characters.
  */
-bool network_sockaddr_sprint(char *, size_t, const struct sockaddr *);
+bool network_sockaddr_sprint(char *, size_t, const struct sockaddr *)
+    __attribute__((__nonnull__));
 
 /*
  * Returns if the addresses from the two sockaddrs are equal.  The ports are
  * ignored, and only AF_INET or AF_INET6 sockaddrs are supported (all others
  * will return false).
  */
-bool network_sockaddr_equal(const struct sockaddr *, const struct sockaddr *);
+bool network_sockaddr_equal(const struct sockaddr *, const struct sockaddr *)
+    __attribute__((__nonnull__));
 
 /* Returns the port number from a sockaddr. */
-unsigned short network_sockaddr_port(const struct sockaddr *);
+unsigned short network_sockaddr_port(const struct sockaddr *)
+    __attribute__((__nonnull__));
 
 /*
  * Compare two addresses relative to an optional mask.  Returns true if
  * they're equal, false otherwise or on a parse error.
  */
-bool network_addr_match(const char *, const char *, const char *mask);
+bool network_addr_match(const char *, const char *, const char *mask)
+    __attribute__((__nonnull__));
 
 /* Set a file descriptor close-on-exec or nonblocking. */
 bool fdflag_close_exec(int fd, bool flag);
@@ -136,19 +150,19 @@ bool fdflag_nonblocking(int fd, bool flag);
  * reporting failures of system calls.
  */
 void debug(const char *, ...)
-    __attribute__((__format__(printf, 1, 2)));
+    __attribute__((__nonnull__, __format__(printf, 1, 2)));
 void notice(const char *, ...)
-    __attribute__((__format__(printf, 1, 2)));
+    __attribute__((__nonnull__, __format__(printf, 1, 2)));
 void sysnotice(const char *, ...)
-    __attribute__((__format__(printf, 1, 2)));
+    __attribute__((__nonnull__, __format__(printf, 1, 2)));
 void warn(const char *, ...)
-    __attribute__((__format__(printf, 1, 2)));
+    __attribute__((__nonnull__, __format__(printf, 1, 2)));
 void syswarn(const char *, ...)
-    __attribute__((__format__(printf, 1, 2)));
+    __attribute__((__nonnull__, __format__(printf, 1, 2)));
 void die(const char *, ...)
-    __attribute__((__noreturn__, __format__(printf, 1, 2)));
+    __attribute__((__nonnull__, __noreturn__, __format__(printf, 1, 2)));
 void sysdie(const char *, ...)
-    __attribute__((__noreturn__, __format__(printf, 1, 2)));
+    __attribute__((__nonnull__, __noreturn__, __format__(printf, 1, 2)));
 
 /*
  * Set the handlers for various message functions.  All of these functions
@@ -166,14 +180,22 @@ void message_handlers_die(int count, ...);
  * handlers take the length of the formatted message, the format, a variadic
  * argument list, and the errno setting if any.
  */
-void message_log_stdout(int, const char *, va_list, int);
-void message_log_stderr(int, const char *, va_list, int);
-void message_log_syslog_debug(int, const char *, va_list, int);
-void message_log_syslog_info(int, const char *, va_list, int);
-void message_log_syslog_notice(int, const char *, va_list, int);
-void message_log_syslog_warning(int, const char *, va_list, int);
-void message_log_syslog_err(int, const char *, va_list, int);
-void message_log_syslog_crit(int, const char *, va_list, int);
+void message_log_stdout(int, const char *, va_list, int)
+    __attribute((__nonnull__));
+void message_log_stderr(int, const char *, va_list, int)
+    __attribute((__nonnull__));
+void message_log_syslog_debug(int, const char *, va_list, int)
+    __attribute((__nonnull__));
+void message_log_syslog_info(int, const char *, va_list, int)
+    __attribute((__nonnull__));
+void message_log_syslog_notice(int, const char *, va_list, int)
+    __attribute((__nonnull__));
+void message_log_syslog_warning(int, const char *, va_list, int)
+    __attribute((__nonnull__));
+void message_log_syslog_err(int, const char *, va_list, int)
+    __attribute((__nonnull__));
+void message_log_syslog_crit(int, const char *, va_list, int)
+    __attribute((__nonnull__));
 
 /* The type of a message handler. */
 typedef void (*message_handler_func)(int, const char *, va_list, int);
@@ -200,34 +222,45 @@ struct cvector {
 };
 
 /* Create a new, empty vector. */
-struct vector *vector_new(void);
-struct cvector *cvector_new(void);
+struct vector *vector_new(void)
+    __attribute__((__malloc__));
+struct cvector *cvector_new(void)
+    __attribute__((__malloc__));
 
 /* Add a string to a vector.  Resizes the vector if necessary. */
-void vector_add(struct vector *, const char *string);
-void cvector_add(struct cvector *, const char *string);
+void vector_add(struct vector *, const char *string)
+    __attribute((__nonnull__));
+void cvector_add(struct cvector *, const char *string)
+    __attribute((__nonnull__));
 
 /* Add a counted string to a vector.  Only available for vectors. */
-void vector_addn(struct vector *, const char *string, size_t length);
+void vector_addn(struct vector *, const char *string, size_t length)
+    __attribute((__nonnull__));
 
 /*
  * Resize the array of strings to hold size entries.  Saves reallocation work
  * in vector_add if it's known in advance how many entries there will be.
  */
-void vector_resize(struct vector *, size_t size);
-void cvector_resize(struct cvector *, size_t size);
+void vector_resize(struct vector *, size_t size)
+    __attribute((__nonnull__));
+void cvector_resize(struct cvector *, size_t size)
+    __attribute((__nonnull__));
 
 /*
  * Reset the number of elements to zero, freeing all of the strings for a
  * regular vector, but not freeing the strings array (to cut down on memory
  * allocations if the vector will be reused).
  */
-void vector_clear(struct vector *);
-void cvector_clear(struct cvector *);
+void vector_clear(struct vector *)
+    __attribute((__nonnull__));
+void cvector_clear(struct cvector *)
+    __attribute((__nonnull__));
 
 /* Free the vector and all resources allocated for it. */
-void vector_free(struct vector *);
-void cvector_free(struct cvector *);
+void vector_free(struct vector *)
+    __attribute((__nonnull__));
+void cvector_free(struct cvector *)
+    __attribute((__nonnull__));
 
 /*
  * Split functions build a vector from a string.  vector_split splits on a
@@ -243,26 +276,34 @@ void cvector_free(struct cvector *);
  * callers of *_split should be prepared for zero-length strings in the
  * vector.
  */
-struct vector *vector_split(const char *string, char sep, struct vector *);
-struct vector *vector_split_space(const char *string, struct vector *);
-struct cvector *cvector_split(char *string, char sep, struct cvector *);
-struct cvector *cvector_split_space(char *string, struct cvector *);
+struct vector *vector_split(const char *string, char sep, struct vector *)
+    __attribute((__nonnull__(1)));
+struct vector *vector_split_space(const char *string, struct vector *)
+    __attribute((__nonnull__(1)));
+struct cvector *cvector_split(char *string, char sep, struct cvector *)
+    __attribute((__nonnull__(1)));
+struct cvector *cvector_split_space(char *string, struct cvector *)
+    __attribute((__nonnull__(1)));
 
 /*
  * Build a string from a vector by joining its components together with the
  * specified string as separator.  Returns a newly allocated string; caller is
  * responsible for freeing.
  */
-char *vector_join(const struct vector *, const char *seperator);
-char *cvector_join(const struct cvector *, const char *separator);
+char *vector_join(const struct vector *, const char *seperator)
+    __attribute__((__malloc__, __nonnull__));
+char *cvector_join(const struct cvector *, const char *separator)
+    __attribute__((__malloc__, __nonnull__));
 
 /*
  * Exec the given program with the vector as its arguments.  Return behavior
  * is the same as execv.  Note the argument order is different than the other
  * vector functions (but the same as execv).
  */
-int vector_exec(const char *path, struct vector *);
-int cvector_exec(const char *path, struct cvector *);
+int vector_exec(const char *path, struct vector *)
+    __attribute((__nonnull__));
+int cvector_exec(const char *path, struct cvector *)
+    __attribute((__nonnull__));
 
 /*
  * The functions are actually macros so that we can pick up the file and line
@@ -298,27 +339,34 @@ int cvector_exec(const char *path, struct cvector *);
  * Last two arguments are always file and line number.  These are internal
  * implementations that should not be called directly.
  */
-void *x_calloc(size_t, size_t, const char *, int);
-void *x_malloc(size_t, const char *, int);
-void *x_realloc(void *, size_t, const char *, int);
-char *x_strdup(const char *, const char *, int);
-char *x_strndup(const char *, size_t, const char *, int);
-int x_vasprintf(char **, const char *, va_list, const char *, int);
+void *x_calloc(size_t, size_t, const char *, int)
+    __attribute__((__alloc_size__(1, 2), __malloc__, __nonnull__));
+void *x_malloc(size_t, const char *, int)
+    __attribute__((__alloc_size__(1), __malloc__, __nonnull__));
+void *x_realloc(void *, size_t, const char *, int)
+    __attribute__((__alloc_size__(2), __malloc__, __nonnull__(3)));
+char *x_strdup(const char *, const char *, int)
+    __attribute__((__malloc__, __nonnull__));
+char *x_strndup(const char *, size_t, const char *, int)
+    __attribute__((__malloc__, __nonnull__));
+int x_vasprintf(char **, const char *, va_list, const char *, int)
+    __attribute__((__nonnull__));
 
 /* asprintf special case. */
 #if HAVE_C99_VAMACROS || HAVE_GNU_VAMACROS
 int x_asprintf(char **, const char *, int, const char *, ...)
-    __attribute__((__format__(printf, 4, 5)));
+    __attribute__((__nonnull__, __format__(printf, 4, 5)));
 #else
 int x_asprintf(char **, const char *, ...)
-    __attribute__((__format__(printf, 2, 3)));
+    __attribute__((__nonnull__, __format__(printf, 2, 3)));
 #endif
 
 /* Failure handler takes the function, the size, the file, and the line. */
 typedef void (*xmalloc_handler_type)(const char *, size_t, const char *, int);
 
 /* The default error handler. */
-void xmalloc_fail(const char *, size_t, const char *, int);
+void xmalloc_fail(const char *, size_t, const char *, int)
+    __attribute__((__nonnull__));
 
 /*
  * Assign to this variable to choose a handler other than the default, which
