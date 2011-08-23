f988381fd3971af1f48ab386a531a2699b764a15
Russ Allbery <rra@stanford.edu>
2011-08-23 16:51:14 -0700

    Probe for sys/time.h and conditionalize its inclusion in network.c
    Windows doesn't have sys/time.h but provides struct timeval from
    winsock2.h, which is included by portable/socket.h.  Probe for
    sys/time.h and include it conditionally.

--- util/network.c
+++ util/network.c
@@ -51,7 +51,9 @@
 #ifdef HAVE_SYS_SELECT_H
 # include <sys/select.h>
 #endif
-#include <sys/time.h>
+#ifdef HAVE_SYS_TIME_H
+# include <sys/time.h>
+#endif
 
 #include <util/fdflag.h>
 #include <util/messages.h>
