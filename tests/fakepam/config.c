27737f17d7bbc1634f3ad018f45f90013c62fa33
Russ Allbery <rra@stanford.edu>
2012-04-29 17:50:00 -0700

    Add regular expression support for PAM tests
    Add regular expression support to the PAM test library.  Both output
    lines and PAM prompts can now be extended regular expressions instead
    of strings, indicated by surrounding them with slashes (/), and the
    actual output or prompt will be matched against the regular
    expression.  If regular expression support is not available, the test
    will be skipped.
    
    The fakepam library now separates the priority from the log line when
    recording output from the PAM module instead of appending a string
    representation of the priority to the beginning of the output line.
    The layout of the output struct (returned by pam_output) has changed
    accordingly.

--- tests/fakepam/config.c
+++ tests/fakepam/config.c
@@ -543,6 +543,7 @@ parse_run(FILE *script)
  * lines in the format:
  *
  *     PRIORITY some output information
+ *     PRIORITY /output regex/
  *
  * where PRIORITY is replaced by the numeric syslog priority corresponding to
  * that priority and the rest of the output undergoes %-esacape expansion.
@@ -551,7 +552,7 @@ parse_run(FILE *script)
 static struct output *
 parse_output(FILE *script, const struct script_config *config)
 {
-    char *line, *token, *piece, *message;
+    char *line, *token, *message;
     struct output *output = NULL;
     int priority;
 
@@ -564,10 +565,8 @@ parse_output(FILE *script, const struct script_config *config)
         token = strtok(NULL, "");
         if (token == NULL)
             bail("malformed line %s", line);
-        piece = expand_string(token, config);
-        basprintf(&message, "%d %s", priority, piece);
-        free(piece);
-        output_add(output, message);
+        message = expand_string(token, config);
+        output_add(output, priority, message);
         free(message);
         free(line);
     }
@@ -580,10 +579,12 @@ parse_output(FILE *script, const struct script_config *config)
  * lines in one of the formats:
  *
  *     type = prompt
+ *     type = /prompt/
  *     type = prompt|response
+ *     type = /prompt/|response
  *
  * If the type is error_msg or info, there is no response.  Otherwise,
- * everything after a colon is taken to be the response that should be
+ * everything after the last | is taken to be the response that should be
  * provided to that prompt.  The response undergoes %-escape expansion.
  */
 static struct prompts *
@@ -591,7 +592,7 @@ parse_prompts(FILE *script, const struct script_config *config)
 {
     struct prompts *prompts = NULL;
     struct prompt *prompt;
-    char *line, *token, *style;
+    char *line, *token, *style, *end;
     size_t size, i, length;
 
     for (line = readline(script); line != NULL; line = readline(script)) {
@@ -622,12 +623,12 @@ parse_prompts(FILE *script, const struct script_config *config)
         if (prompt->style == PAM_ERROR_MSG || prompt->style == PAM_TEXT_INFO)
             prompt->prompt = expand_string(token, config);
         else {
-            token = strtok(token, "|");
-            prompt->prompt = expand_string(token, config);
-            token = strtok(NULL, "");
-            if (token == NULL)
+            end = strrchr(token, '|');
+            if (end == NULL)
                 bail("malformed prompt line near %s", prompt->prompt);
-            token = skip_whitespace(token);
+            *end = '\0';
+            prompt->prompt = expand_string(token, config);
+            token = end + 1;
             prompt->response = expand_string(token, config);
         }
         prompts->size++;
