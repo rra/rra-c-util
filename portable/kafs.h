b29751f3f4ffaf4398c62ca4b3f533b2a0cfaa76
Russ Allbery <rra@stanford.edu>
2013-06-02 13:17:04 -0700

    Update copyright statement for portable/kafs.h

--- portable/kafs.h
+++ portable/kafs.h
@@ -16,7 +16,7 @@
  * which can be found at <http://www.eyrie.org/~eagle/software/rra-c-util/>.
  *
  * Written by Russ Allbery <rra@stanford.edu>
- * Copyright 2006, 2007, 2008, 2010
+ * Copyright 2006, 2007, 2008, 2010, 2013
  *     The Board of Trustees of the Leland Stanford Junior University
  *
  * Permission is hereby granted, free of charge, to any person obtaining a
