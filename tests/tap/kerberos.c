cda482b40a72f9262b984e5bd2b02024f9fca6bb
Russ Allbery <eagle@eyrie.org>
2014-12-25 15:16:39 -0800

    Add optional PKINIT configuration to kerberos_setup test function
    In the TAP add-on for Kerberos testing, add a new flag,
    TAP_KRB_NEEDS_PKINIT, to the kerberos_setup function.  This indicates
    that configuration for PKINIT authentications is required for this
    test case.

--- tests/tap/kerberos.c
+++ tests/tap/kerberos.c
@@ -219,6 +219,8 @@ kerberos_free(void)
         free(config->userprinc);
         free(config->username);
         free(config->password);
+        free(config->pkinit_principal);
+        free(config->pkinit_cert);
         free(config);
         config = NULL;
     }
@@ -349,6 +351,31 @@ kerberos_setup(enum kerberos_needs needs)
         config->realm++;
     }
     test_file_path_free(path);
+
+    /*
+     * If we have PKINIT configuration, read it and fill out the relevant
+     * members of our config struct.
+     */
+    path = test_file_path("config/pkinit-principal");
+    if (path != NULL)
+        file = fopen(path, "r");
+    if (file != NULL) {
+        if (fgets(buffer, sizeof(buffer), file) == NULL)
+            bail("cannot read %s", path);
+        if (buffer[strlen(buffer) - 1] != '\n')
+            bail("no newline in %s", path);
+        buffer[strlen(buffer) - 1] = '\0';
+        fclose(file);
+        test_file_path_free(path);
+        path = test_file_path("config/pkinit-cert");
+        if (path != NULL) {
+            config->pkinit_principal = bstrdup(buffer);
+            config->pkinit_cert = bstrdup(path);
+        }
+    }
+    test_file_path_free(path);
+    if (config->pkinit_cert == NULL && (needs & TAP_KRB_NEEDS_PKINIT) != 0)
+        skip_all("PKINIT tests not configured");
 
     /*
      * Register the cleanup function so that the caller doesn't have to do
