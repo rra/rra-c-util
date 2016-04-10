da7aae962cf4b3f4cb2a0f7eb6a2ce3ad9b53445
Russ Allbery <eagle@eyrie.org>
2016-04-10 11:32:34 -0700

    Eliminate a useless variable in some vector functions
    This avoids dereferencing vector->count before the assert that
    vector isn't NULL, which cleans up cppcheck warnings.  Thanks,
    Julien ÉLIE.

--- util/vector.c
+++ util/vector.c
@@ -113,24 +113,20 @@ cvector_resize(struct cvector *vector, size_t size)
 void
 vector_add(struct vector *vector, const char *string)
 {
-    size_t next = vector->count;
-
     assert(vector != NULL);
     if (vector->count == vector->allocated)
         vector_resize(vector, vector->allocated + 1);
-    vector->strings[next] = xstrdup(string);
+    vector->strings[vector->count] = xstrdup(string);
     vector->count++;
 }
 
 void
 cvector_add(struct cvector *vector, const char *string)
 {
-    size_t next = vector->count;
-
     assert(vector != NULL);
     if (vector->count == vector->allocated)
         cvector_resize(vector, vector->allocated + 1);
-    vector->strings[next] = string;
+    vector->strings[vector->count] = string;
     vector->count++;
 }
 
@@ -144,12 +140,10 @@ cvector_add(struct cvector *vector, const char *string)
 void
 vector_addn(struct vector *vector, const char *string, size_t length)
 {
-    size_t next = vector->count;
-
     assert(vector != NULL);
     if (vector->count == vector->allocated)
         vector_resize(vector, vector->allocated + 1);
-    vector->strings[next] = xstrndup(string, length);
+    vector->strings[vector->count] = xstrndup(string, length);
     vector->count++;
 }
 
