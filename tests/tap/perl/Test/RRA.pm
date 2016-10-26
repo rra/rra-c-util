3e282bfe25714f4abdab39c23effd08eec567c69
Russ Allbery <eagle@eyrie.org>
2016-10-25 21:18:40 -0700

    Add is_file_contents function to Test::RRA
    Add new is_file_contents function to Test::RRA for Perl tests.  This
    compares a string with the contents of a file and displays a diff if
    they don't match.

--- tests/tap/perl/Test/RRA.pm
+++ tests/tap/perl/Test/RRA.pm
@@ -13,6 +13,7 @@ use strict;
 use warnings;
 
 use Exporter;
+use File::Temp;
 use Test::More;
 
 # For Perl 5.006 compatibility.
@@ -26,12 +27,49 @@ our (@EXPORT_OK, @ISA, $VERSION);
 # consistency is good).
 BEGIN {
     @ISA       = qw(Exporter);
-    @EXPORT_OK = qw(skip_unless_author skip_unless_automated use_prereq);
+    @EXPORT_OK = qw(
+      is_file_contents skip_unless_author skip_unless_automated use_prereq
+    );
 
     # This version should match the corresponding rra-c-util release, but with
     # two digits for the minor version, including a leading zero if necessary,
     # so that it will sort properly.
     $VERSION = '6.02';
+}
+
+# Compare a string to the contents of a file, similar to the standard is()
+# function, but to show the line-based unified diff between them if they
+# differ.
+#
+# $got      - The output that we received
+# $expected - The path to the file containing the expected output
+# $message  - The message to use when reporting the test results
+#
+# Returns: undef
+#  Throws: Exception on failure to read or write files or run diff
+sub is_file_contents {
+    my ($got, $expected, $message) = @_;
+
+    # If they're equal, this is simple.
+    open(my $fh, '<', $expected) or BAIL_OUT("Cannot open $expected: $!\n");
+    my $data = do { local $/ = undef; <$fh> };
+    close($fh) or BAIL_OUT("Cannot close $expected: $!\n");
+    if ($got eq $data) {
+        is($got, $data, $message);
+        return;
+    }
+
+    # They're not equal.  Write out what we got so that we can run diff.
+    my ($tmp, $tmpname) = File::Temp->new();
+    print {$tmp} $got or BAIL_OUT("Cannot write to $tmpname: $!\n");
+
+    # Run diff and report the results.
+    my $diff = capturex([0 .. 1], 'diff', '-u', $expected, 'tmp');
+    diag($diff);
+
+    # Remove the temporary file and report failure.
+    ok(0, $message);
+    return;
 }
 
 # Skip this test unless author tests are requested.  Takes a short description
