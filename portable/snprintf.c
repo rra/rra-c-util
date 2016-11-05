155b8f9d4a8e39c2f260e6bafe572935d8552748
Russ Allbery <eagle@eyrie.org>
2016-11-04 21:36:05 -0700

    Work around warning bug in older gcc with snprintf
    Change a variable to unsigned in the hope that this will work
    around a bug in the gcc used by Travis-CI with -O2 warning
    analysis.

--- portable/snprintf.c
+++ portable/snprintf.c
@@ -181,7 +181,7 @@ static int dopr (char *buffer, size_t maxlen, const char *format, va_list args)
   char *strvalue;
   int min;
   int max;
-  int state;
+  unsigned int state;
   int flags;
   int cflags;
   int total;
