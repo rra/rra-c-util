aa25877580a914550da00d69d4a392333840bd98
Russ Allbery <eagle@eyrie.org>
2018-06-23 16:48:54 -0700

    Bump Perl module versions

--- tests/tap/perl/Test/RRA/ModuleVersion.pm
+++ tests/tap/perl/Test/RRA/ModuleVersion.pm
@@ -33,7 +33,7 @@ BEGIN {
     # This version should match the corresponding rra-c-util release, but with
     # two digits for the minor version, including a leading zero if necessary,
     # so that it will sort properly.
-    $VERSION = '7.02';
+    $VERSION = '7.03';
 }
 
 # A regular expression matching the version string for a module using the
