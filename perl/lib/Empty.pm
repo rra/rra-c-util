369591899b7c22d9583bc030c3fad27f158f697b
Russ Allbery <eagle@eyrie.org>
2020-05-10 18:46:58 -0700

    Check the version of the embedded Empty module
    rra-c-util had special logic in the perl/module-version-t test to
    check the embeded tests/tap/perl support modules instead of other
    embedded modules.  The "instead of" part was wrong; it should have
    been "in addition to."  Fix that by checking the entire rra-c-util
    source tree for Perl modules whose version needs to be updated.

--- perl/lib/Empty.pm
+++ perl/lib/Empty.pm
@@ -19,7 +19,7 @@ our $VERSION;
 # against circular module loading (not that we load any modules, but
 # consistency is good).
 BEGIN {
-    $VERSION = '8.01';
+    $VERSION = '8.02';
 }
 
 # Empty function for testing purposes.
