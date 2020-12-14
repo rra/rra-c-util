108863aaff7b306fa36a70bdb0aabfae00881ef2
Russ Allbery <eagle@eyrie.org>
2020-12-13 20:15:27 -0800

    Rename KRB5_CPPFLAGS_GCC to KRB5_CPPFLAGS_WARNINGS
    Rename the KRB5_CPPFLAGS_GCC output variable from the RRA_LIB_KRB5
    macro to KRB5_CPPFLAGS_WARNINGS to more accurately reflect its
    intended use and to reflect that it works with Clang.

--- perl/lib/Empty.pm
+++ perl/lib/Empty.pm
@@ -19,7 +19,7 @@ our $VERSION;
 # against circular module loading (not that we load any modules, but
 # consistency is good).
 BEGIN {
-    $VERSION = '8.04';
+    $VERSION = '9.00';
 }
 
 # Empty function for testing purposes.
